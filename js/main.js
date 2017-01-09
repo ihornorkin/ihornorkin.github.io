2419bff559e089f0dfdceda259aa24c007a3c42f
ihornorkin <ihornorkin@gmail.com>
2017-01-09 19:44:54 +0200

    Add new work and show canvas

--- js/main.js
+++ js/main.js
@@ -1,89 +1,8 @@
-//Форма отправки 2.0 //
-$(function() {
-  $('.button-send').click(function() {
-    var message = $('.message').html();
-    $('.message-input').val(message);
-  });
-
-  $("[name=send]").click(function () {
-    $(":input.error").removeClass('error');
-    $(".allert").remove();
-
-    var error;
-    var btn = $(this);
-    var ref = btn.closest('form').find('[required]');
-    var msg = btn.closest('form').find('input');
-    var send_btn = btn.closest('form').find('[name=send]');
-    var send_options = btn.closest('form').find('[name=campaign_token]');
-
-    $(ref).each(function() {
-      if ($(this).val() == '') {
-        var errorfield = $(this);
-        $(this).addClass('error').parent('.field').append('<div class="allert"><span>Required field</span></div>');
-        error = 1;
-        $(":input.error:first").focus();
-        return;
-      } else {
-        var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-        if ($(this).attr("type") == 'email') {
-          if(!pattern.test($(this).val())) {
-            $("[name=email]").val('');
-            $(this).addClass('error').parent('.field').append('<div class="allert"><span>Input correct e-mail</span></div>');
-            error = 1;
-            $(":input.error:first").focus();
-          }
-        }
-      }
-    });
-    if(!(error==1)) {
-      $(send_btn).each(function() {
-        $(this).attr('disabled', true);
-      });
-      $(send_options).each(function() {
-        if ($(this).val() == '') {
-          $.ajax({
-            type: 'POST',
-            url: 'mail.php',
-            data: msg,
-            success: function() {
-              setTimeout(function(){ $('form').trigger("reset");
-                $("[name=send]").removeAttr("disabled"); }, 1000);
-                           },
-                           error: function(xhr, str) {
-                            alert('Error' + xhr.responseCode);
-                          }
-                        });
-        } else {
-          $.ajax({
-            type: 'POST',
-            url: 'mail.php',
-            data: msg,
-            success:
-            $.ajax({
-              type: 'POST',
-              url: 'https://app.getresponse.com/add_subscriber.html',
-              data: msg,
-              statusCode: {0:function() {
-                setTimeout(function(){ $('form').trigger("reset");
-                  $("[name=send]").removeAttr("disabled"); }, 1000);
-                                  }}
-                                }),
-            error:  function(xhr, str) {
-              alert('Error' + xhr.responseCode);
-            }
-          });
-        }
-      });
-    }
-    return false;
-  })
-});
-
 /* Entering your JS code here */
 
 $(document).ready(function(){
 
-  new WOW().init();
+ // new WOW().init();
 
   $("body").on("click",".custome-link", function (event) {
     event.preventDefault();
@@ -116,6 +35,15 @@ $(document).ready(function(){
       }
     }
 
+    $(window).keydown(function() {
+      if (event.keyCode == 27) {
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        $('#cbp-spmenu-s1').removeClass('cbp-spmenu-open');
+        isClosed = true;
+      }
+    });
+
   });
 
   $('#show-about-me').click(function() {
@@ -124,30 +52,24 @@ $(document).ready(function(){
     ($("#show-content").text() === "Show more") ? $("#show-content").text("Less about me") : $("#show-content").text("Show more");
   });
 
-  function openModal(selector) {
-    var modalData = '[data-window="' + selector + '"]';
-    var openModal = '#' + selector;
-    $(modalData).click(function() {
-      $(openModal).addClass('show-works');
+  function openModal() {
+    $('#portfolio .item').click(function() {
+      var selector = '#' + $(this).attr('data-window');
+      $(selector).addClass('show-works');
       $('body').addClass('open-works');
       setTimeout(function() {
-        $(openModal + ' .descope').removeClass('image-animate');
+        $(selector).find('.descope').removeClass('image-animate');
       }, 600);
       setTimeout(function() {
-        $(openModal + ' .mobile').removeClass('image-animate');
+        $(selector).find('.mobile').removeClass('image-animate');
       }, 640);
       setTimeout(function() {
-        $(openModal + ' .tablet').removeClass('image-animate');
+        $(selector).find('.tablet').removeClass('image-animate');
       }, 680);
     });
-  };
+  }
 
-  openModal('portfolio-one');
-  openModal('portfolio-two');
-  openModal('portfolio-three');
-  openModal('portfolio-four');
-  openModal('portfolio-five');
-  openModal('portfolio-six');
+  openModal();
 
   /* Close modal */
 
